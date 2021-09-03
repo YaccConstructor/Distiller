205079f4bfed05f8a7686dcf25cde187c7cfa695
gsv <rsdpisuy@gmail.com>
2021-09-03 15:15:28 +0300

    Check that distiller is optimizer.

--- test/Test/DistilledProgramsTest.hs
+++ test/Test/DistilledProgramsTest.hs
@@ -19,9 +19,17 @@ import Helpers
 import Control.Exception
 import Text.Printf (printf)
 import Data.Maybe (fromJust, isJust)
+import qualified Data.Foldable as List
 
+substituteAll :: Foldable t => Term -> t (String, Term) -> Term
+substituteAll =
+    foldl (\ term (name, subst_term) -> subst subst_term (abstract term name)) 
 
-createTest :: String -> String -> ((Term, [(String, ([String], Term))]) -> (Term, [(String, ([String], Term))]) -> Property) -> p -> IO TestTree
+createTest :: String 
+              -> String 
+              -> ((Term, [(String, ([String], Term))]) -> (Term, [(String, ([String], Term))]) -> (Property, Property)) 
+              -> p 
+              -> IO TestTree
 createTest fileToDistill importsForDistill makePropertyTest timeoutForDistillation =  do
   progToDistill <- load fileToDistill importsForDistill  
   if isJust progToDistill
@@ -33,8 +41,13 @@ createTest fileToDistill importsForDistill makePropertyTest timeoutForDistillati
         let assertion = assertFailure $ printf "program: %s; imports: %s; exception: %s" fileToDistill importsForDistill (show ex)
         return $ testCase testCaseName assertion
       Right  distilled -> do
-        let testCaseName = printf "%s: evaluation results of original and distilled versions are equal." fileToDistill 
-        return $ testProperty testCaseName $ makePropertyTest (fromJust progToDistill) distilled
+        let (distillerIsCorrect, distillerIsOptimizer) = makePropertyTest (fromJust progToDistill) distilled
+        let distillerIsCorrectTestCaseName = printf "%s: evaluation results of original and distilled programs are equal" fileToDistill 
+        let distillerIsOptimizerTestCaseName = printf "%s: evaluation of the distilled program requires less resources than evaluation of the original one" fileToDistill 
+        return $ testGroup 
+                    fileToDistill 
+                    [ testProperty distillerIsCorrectTestCaseName distillerIsCorrect,
+                      testProperty distillerIsOptimizerTestCaseName distillerIsOptimizer]
   else do
     let testCaseName = printf "Parsing: %s" fileToDistill
     let assertion = assertFailure $ printf "program: %s; imports: %s." fileToDistill importsForDistill
@@ -42,15 +55,45 @@ createTest fileToDistill importsForDistill makePropertyTest timeoutForDistillati
 
 
 test_plusMinus_2_property :: IO TestTree
-test_plusMinus_2_property = do
-    let plusMinus_2 (origMainTerm, origTerms) (distilledMainTerm, distilledTerms) =     
-         property $ do  n <- forAll genNat
-                        m <- forAll genNat
-                        let n_term = natToTerm n
-                        let m_term = natToTerm m
-                        let (origRes,origReductions,origAlloc) = Term.eval (subst m_term (abstract (subst n_term (abstract origMainTerm "n")) "m")) EmptyCtx origTerms 0 0
-                        let (distilledRes, distilledReductions, distilledAlloc) = Term.eval (subst m_term (abstract (subst n_term (abstract distilledMainTerm "n")) "m")) EmptyCtx distilledTerms 0 0
-                        origRes === distilledRes
-    
-    createTest "plusMinus_2" "inputs/" plusMinus_2 defaultTimeout 
+test_plusMinus_2_property = do createTest "plusMinus_2" "inputs/" plusMinus_2 defaultTimeout 
+    where
+    plusMinus_2 (origMainTerm, origTerms) (distilledMainTerm, distilledTerms) = do (p1,p2)
+        where 
+        getEvalResults m n = let n_term = natToTerm n
+                                 m_term = natToTerm m
+                                 substitutions = [("n", n_term), ("m", m_term)]
+                                 origResults = Term.eval (substituteAll origMainTerm substitutions) EmptyCtx origTerms 0 0            
+                                 distilledResults = Term.eval (substituteAll distilledMainTerm substitutions)  EmptyCtx distilledTerms 0 0
+                             in (origResults, distilledResults)
+
+        p1 = property $ do  n <- forAll genNat
+                            m <- forAll genNat
+                            let ((origRes, _1,_2), (distilledRes, _3,_4)) = getEvalResults m n
+                            origRes === distilledRes
+        p2 = property $ do  n <- forAll genNat
+                            m <- forAll genNat
+                            let ((_1, origReductions, origAllocations), (_2, distilledReductions,distilledAllocations)) = getEvalResults m n
+                            (origReductions >= distilledReductions && origAllocations >= distilledAllocations) === True
+        
+
+test_plusMinus_1_property :: IO TestTree
+test_plusMinus_1_property = do createTest "plusMinus_1" "inputs/" plusMinus_2 defaultTimeout 
+    where
+    plusMinus_2 (origMainTerm, origTerms) (distilledMainTerm, distilledTerms) = do (p1,p2)
+        where 
+        getEvalResults m n = let n_term = natToTerm n
+                                 m_term = natToTerm m
+                                 substitutions = [("n", n_term), ("m", m_term)]
+                                 origResults = Term.eval (substituteAll origMainTerm substitutions) EmptyCtx origTerms 0 0            
+                                 distilledResults = Term.eval (substituteAll distilledMainTerm substitutions)  EmptyCtx distilledTerms 0 0
+                             in (origResults, distilledResults)
+
+        p1 = property $ do  n <- forAll genNat
+                            m <- forAll genNat
+                            let ((origRes, _1,_2), (distilledRes, _3,_4)) = getEvalResults m n
+                            origRes === distilledRes
+        p2 = property $ do  n <- forAll genNat
+                            m <- forAll genNat
+                            let ((_1, origReductions, origAllocations), (_2, distilledReductions,distilledAllocations)) = getEvalResults m n
+                            (origReductions >= distilledReductions && origAllocations >= distilledAllocations) === True    
     

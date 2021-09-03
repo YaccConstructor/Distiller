9a681f69134bf5d9b171075246b601b1310aea0b
gsv <rsdpisuy@gmail.com>
2021-09-03 14:30:02 +0300

    First property based test for distller.

---
+++ test/Test/TestHelpers.hs
@@ -0,0 +1,26 @@
+module Test.TestHelpers where
+
+import Helpers
+import Test.Tasty
+import Test.Tasty.HUnit
+import Data.Maybe (fromJust, isJust)
+import Term
+import Trans (dist)
+
+defaultTimeout :: Integer
+defaultTimeout = 2 * 1000000 --timeout in nanoseconds: 1 sec = 10^6 ns 
+
+_10sec :: Integer
+_10sec = 10 * 1000000
+
+_1min :: Integer
+_1min = 60 * 1000000
+
+timeOutTest :: Integer -> TestName -> Assertion -> TestTree
+timeOutTest timeout testName assertion =
+  localOption (mkTimeout timeout) $ testCase testName assertion
+
+load :: String -> String -> IO (Maybe (Term, [(String, ([String], Term))]))
+load root imports = loadProg [root] [] [] $ Just imports
+
+

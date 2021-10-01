717a0179b0e74687a54d1b70b4e839a929b55e6d
gsv <rsdpisuy@gmail.com>
2021-10-01 12:26:45 +0300

    More examples of linear algebra distillation.

--- test/Test/DistilledProgramsTest.hs
+++ test/Test/DistilledProgramsTest.hs
@@ -124,7 +124,7 @@ createRealWorldTest fileToDistill importsForDistill getEvaluationResults =  do
     return $ testCase testCaseName assertion
 
 
-test_matrices_add_add_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/addadd" "inputs/" getEvaluationResults
+test_matrices_add_add_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/addAdd" "inputs/" getEvaluationResults
     where
     getEvaluationResults origProg distilledProg = do          
         m <- loadFileToTerm "inputs/data/Football_64x64.pot"
@@ -132,6 +132,25 @@ test_matrices_add_add_football_football_64x64 = do createRealWorldTest "linearAl
           Left e -> return $ Left e            
           Right u -> 
             return $ Right $ getEvalResults [("m1", u), ("m2", u), ("m3", u)] origProg distilledProg
+
+test_matrices_add_add_add_1_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/addAddAdd1" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        case m of 
+          Left e -> return $ Left e            
+          Right u -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", u), ("m3", u), ("m4", u)] origProg distilledProg
+
+test_matrices_add_add_add_2_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/addAddAdd2" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        case m of 
+          Left e -> return $ Left e            
+          Right u -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", u), ("m3", u), ("m4", u)] origProg distilledProg
+
 
 test_matrices_add_kron_football_64x64_small_2x2 = do createRealWorldTest "linearAlgebraExamples/addKron2" "inputs/" getEvaluationResults
     where
@@ -144,7 +163,8 @@ test_matrices_add_kron_football_64x64_small_2x2 = do createRealWorldTest "linear
           (Right u, Right v) -> 
             return $ Right $ getEvalResults [("m1", u), ("m2", u), ("m3", v)] origProg distilledProg
 
---test_matrices_add_kron_kron_football_64x64_small_2x2 = do createRealWorldTest "linearAlgebraExamples/addKron1" "inputs/" getEvaluationResults
+--test_matrices_add_kron_kron_football_64x64_small_2x2 = do test <- createRealWorldTest "linearAlgebraExamples/addKron1" "inputs/" getEvaluationResults
+--                                                          return $ ignoreTest test    
 --    where
 --    getEvaluationResults origProg distilledProg = do          
 --        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
@@ -156,11 +176,60 @@ test_matrices_add_kron_football_64x64_small_2x2 = do createRealWorldTest "linear
 --            return $ Right $ getEvalResults [("m1", u), ("m2", u), ("m3", v), ("m4", v)] origProg distilledProg
 
 
---test_matrices_map_add_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/mapAdd" "inputs/" getEvaluationResults
---    where
---    getEvaluationResults origProg distilledProg = do          
---        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
---        case m of 
---          Left e -> return $ Left e            
---          Right u -> 
---            return $ Right $ getEvalResults [("m1", u), ("m2", u)] origProg distilledProg
+test_matrices_map_add_football_football_64x64 = do createRealWorldTest "linearAlgebraExamples/mapAdd" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        case m of 
+          Left e -> return $ Left e            
+          Right u -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", u)] origProg distilledProg
+
+test_matrices_map_kron_football_64x64_small_1_2x2 = do createRealWorldTest "linearAlgebraExamples/mapKron" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        n <- loadFileToTerm "inputs/data/Small_1_2x2.pot"
+        case (m,n) of 
+          (Left e,x) -> return $ Left e            
+          (x, Left e) -> return $ Left e            
+          (Right u, Right v) -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", v)] origProg distilledProg
+
+test_matrices_kron_mask_football_64x64_small_1_2x2_mask1 = do createRealWorldTest "linearAlgebraExamples/kronMask" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        n <- loadFileToTerm "inputs/data/Small_1_2x2.pot"
+        msk <- loadFileToTerm "inputs/data/Mask1.pot"
+        case (m,n,msk) of 
+          (Left e,x,y) -> return $ Left e
+          (x, Left e,y) -> return $ Left e
+          (x, y, Left e) -> return $ Left e
+          (Right u, Right v, Right msk) -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", v), ("msk", msk)] origProg distilledProg
+
+test_matrices_kron_mask_football_64x64_small_1_2x2_mask2 = do createRealWorldTest "linearAlgebraExamples/kronMask" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"
+        n <- loadFileToTerm "inputs/data/Small_1_2x2.pot"
+        msk <- loadFileToTerm "inputs/data/Mask2.pot"
+        case (m,n,msk) of 
+          (Left e,x,y) -> return $ Left e
+          (x, Left e,y) -> return $ Left e
+          (x, y, Left e) -> return $ Left e
+          (Right u, Right v, Right msk) -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", v), ("msk", msk)] origProg distilledProg
+
+
+test_matrices_add_mask_football_football_64x64_mask1 = do createRealWorldTest "linearAlgebraExamples/addMask" "inputs/" getEvaluationResults
+    where
+    getEvaluationResults origProg distilledProg = do          
+        m <- loadFileToTerm "inputs/data/Football_64x64.pot"        
+        msk <- loadFileToTerm "inputs/data/Mask1.pot"
+        case (m,msk) of 
+          (Left e,x) -> return $ Left e
+          (x, Left e) -> return $ Left e          
+          (Right u, Right msk) -> 
+            return $ Right $ getEvalResults [("m1", u), ("m2", u), ("msk", msk)] origProg distilledProg

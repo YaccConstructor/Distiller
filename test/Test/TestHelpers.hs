02b85189ce30c1036014e6a764ff2f108450d333
gsv <rsdpisuy@gmail.com>
2021-10-01 12:39:09 +0300

    Linear algebra examples moved to separated file.

--- test/Test/TestHelpers.hs
+++ test/Test/TestHelpers.hs
@@ -27,4 +27,18 @@ loadFileToTerm file = do
     str <- readFile file
     return $ parseTerm str
 
+substituteAll :: Foldable t => Term -> t (String, Term) -> Term
+substituteAll =
+    foldl (\ term (name, subst_term) -> subst subst_term (abstract term name)) 
+
+getEvalResults :: (Num a1, Foldable t1, Num a2, Foldable t3, Foldable t2) =>
+                  t2 (String, Term)
+                  -> (Term, [(String, (t3 String, Term))])
+                  -> (Term, [(String, (t1 String, Term))])
+                  -> ((Term, Int, a2), (Term, Int, a1))
+getEvalResults substitutions (origMainTerm, origTerms) (distilledMainTerm, distilledTerms) =         
+    let origResults = Term.eval (substituteAll origMainTerm substitutions) EmptyCtx origTerms 0 0            
+        distilledResults = Term.eval (substituteAll distilledMainTerm substitutions)  EmptyCtx distilledTerms 0 0
+    in (origResults, distilledResults)    
+
 

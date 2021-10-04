a0eace4be2b76f7423851aba1ee7170b927a0b35
katevi <catherine.vinnik@gmail.com>
2021-10-04 23:27:39 +0300

    Remove prog evaluation impl temporarily

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -6,7 +6,7 @@ import Generalizer
 import Data.List
 
   
-eval (Free x) k d r a = error ("Unbound identifier: "++x)
+{---eval (Free x) k d r a = error ("Unbound identifier: "++x)
 eval (Lambda x t) EmptyCtx d r a = (Lambda x t,r,a)
 eval (Lambda x t) (ApplyCtx k u) d r a = eval (subst u t) k d (r+1) a
 eval (Lambda x t) (CaseCtx k bs) d r a = error ("Unapplied function in case selector: " ++ show (Lambda x t))
@@ -22,4 +22,4 @@ eval (Fun f) k d r a = case lookup f d of
                           Nothing -> error ("Undefined function: "++f)
                           Just (xs,t)  -> eval (foldr Lambda t xs) k d (r+1) a
 eval (Case t bs) k d r a = eval t (CaseCtx k bs) d r a
-eval (Let x t u) k d r a = eval (subst t u) k d (r+1) a
+eval (Let x t u) k d r a = eval (subst t u) k d (r+1) a --}

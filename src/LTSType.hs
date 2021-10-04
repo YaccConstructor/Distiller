0de951ede41fc88e3a88f2b036e875b8b9d5c4f7
katevi <catherine.vinnik@gmail.com>
2021-10-04 23:25:52 +0300

    Change LTS types defs to recursive ways

--- src/LTSType.hs
+++ src/LTSType.hs
@@ -7,22 +7,26 @@ import TermType
 
 data LTS = Leaf Term | LTS LTSTransitions
 
-data LTSTransitions = LTSTransitions Term [(String, Term)]
+data LTSTransitions = LTSTransitions Term [(String, LTS)]
 
 doLTS0Tr :: Term -> LTS
 doLTS0Tr = Leaf  
 
-doLTS1Tr :: Term -> String -> Term -> LTS
+doLTS1Tr :: Term -> String -> LTS -> LTS
 doLTS1Tr oldTerm label newTerm = LTS $ LTSTransitions oldTerm [(label, newTerm)]
 
-doLTSManyTr :: Term -> [(String, Term)] -> LTS
+doLTSManyTr :: Term -> [(String, LTS)] -> LTS
 doLTSManyTr oldTerm pairs = LTS $ LTSTransitions oldTerm pairs
 
 getLabels :: LTSTransitions -> [String]
 getLabels (LTSTransitions _ pairs) = map fst pairs
 
 getNewTerms :: LTSTransitions -> [Term]
-getNewTerms (LTSTransitions _ pairs) = map snd pairs
+getNewTerms (LTSTransitions _ pairs) = map (\(_, y) -> 
+    case y of
+        (Leaf term) -> term
+        (LTS (LTSTransitions tr _)) -> tr
+        ) pairs
 
 getOldTerm :: LTSTransitions -> Term
 getOldTerm (LTSTransitions oldTerm _) = oldTerm

0480c1102d01f4e525a9a6860e86f7e7f39bc3c1
katevi <catherine.vinnik@gmail.com>
2022-01-21 21:38:31 +0300

    Redo homeomorphic embedding check implementation with list comprehension, all 26 tests pass

--- src/HomeomorphicEmbeddingChecker.hs
+++ src/HomeomorphicEmbeddingChecker.hs
@@ -64,88 +64,84 @@ isRenaming' funNamesAccum (LTS (LTSTransitions _ ((Case', t) : branches)))
             in isRenaming' funNamesAccum u u' freeVars' (xs'' ++ boundVars) renaming') initializer branchess
 isRenaming' _ t u _ _ renaming = []
 
-isHomeomorphicEmbedding lts1 lts2 = sort $ nub $ isHomeomorphicEmbedding' lts1 lts2
+isHomeomorphicEmbedding lts1 lts2 = isHomeomorphicEmbedding' lts1 lts2
 
 isHomeomorphicEmbedding' :: LTS -> LTS -> [(String, String)]
 --isHomeomorphicEmbedding' t u | traceShow ("isHEmbedding: t = " ++ show t ++ " and u = " ++ show u) False = undefined
-isHomeomorphicEmbedding' lts1 lts2 = coupled [] lts1 lts2 [] [] []
+isHomeomorphicEmbedding' lts1 lts2 = concat $ coupled [] lts1 lts2 [] [] []
 
-embed ::  [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [(String, String)]
-embed funNamesAccum lts1 lts2 freeVars boundVars renaming | traceShow ("embed: t = " ++ show lts1 ++ " and u = "
-    ++ show lts2 ++ ", dived = " ++ show (dived funNamesAccum lts1 lts2 freeVars boundVars renaming)
-    ++ show (coupled funNamesAccum lts1 lts2 freeVars boundVars renaming)) False = undefined
+embed ::  [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [[(String, String)]]
+--embed funNamesAccum lts1 lts2 freeVars boundVars renaming | traceShow ("embed: t = " ++ show lts1 ++ " and u = "
+  --  ++ show lts2 ++ ", dived = " ++ show (dived funNamesAccum lts1 lts2 freeVars boundVars renaming)
+   -- ++ show (coupled funNamesAccum lts1 lts2 freeVars boundVars renaming)) False = undefined
 embed funNamesAccum lts1 lts2 freeVars boundVars renaming =
   sort $ nub (coupled funNamesAccum lts1 lts2 freeVars boundVars renaming ++ dived funNamesAccum lts1 lts2 freeVars boundVars renaming)
 
-dived ::  [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [(String, String)]
-dived _ t u _ _ _ | traceShow ("dived : t = " ++ show t ++ " and u = " ++ show u) False = undefined
+dived ::  [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [[(String, String)]]
+--dived _ t u _ _ _ | traceShow ("dived : t = " ++ show t ++ " and u = " ++ show u) False = undefined
 --dived funNamesAccum (LTS (LTSTransitions (Free x) _)) (LTS (LTSTransitions _ (_ : _))) _ _ _ = []
 dived funNamesAccum lts1
-                    lts2@(LTS (LTSTransitions _ branches'@((Unfold' _, _) : _))) freeVars boundVars renaming =
-    -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
+                    lts2@(LTS (LTSTransitions _ branches'@([(Unfold' _, t)]))) freeVars boundVars renaming =
+                      embed funNamesAccum lts1 t freeVars boundVars renaming
 dived funNamesAccum lts1
-                    lts2@(LTS (LTSTransitions _ branches'@((Lambda' _, _) : _))) freeVars boundVars renaming =
+                    lts2@(LTS (LTSTransitions _ branches'@([(Lambda' x, t)]))) freeVars boundVars renaming =
     -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
+     embed funNamesAccum lts1 t freeVars boundVars renaming
 dived funNamesAccum lts1
                     lts2@(LTS (LTSTransitions _ branches'@((Con' _, _) : _))) freeVars boundVars renaming =
     -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
+    concat [embed funNamesAccum lts1 t' freeVars boundVars renaming | t' <- map snd branches']
 {---dived funNamesAccum lts1@(LTS (LTSTransitions _ branches@((Apply0', LTS (LTSTransitions (Apply (Fun f) _) _)) : _)))
                     lts2@(LTS (LTSTransitions _ branches'@((Apply0', LTS (LTSTransitions (Apply (Fun g) _) _)) : _))) freeVars boundVars renaming
                     | f /= g = [] --}
 dived funNamesAccum lts1
-                    lts2@(LTS (LTSTransitions _ branches'@((Apply0', _)  : _))) freeVars boundVars renaming =
+                    lts2@(LTS (LTSTransitions _ branches'@([(Apply0', t), (Apply1', u)]))) freeVars boundVars renaming =
     -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
+    embed funNamesAccum lts1 t freeVars boundVars renaming ++ embed funNamesAccum lts1 u freeVars boundVars renaming
 dived funNamesAccum lts1
-                    lts2@(LTS (LTSTransitions _ branches'@((Case', _) : _))) freeVars boundVars renaming =
+                    lts2@(LTS (LTSTransitions _ ((Case', t) : branches))) freeVars boundVars renaming =
     -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
+    embed funNamesAccum lts1 t freeVars boundVars renaming ++ concatMap (\(CaseBranch' c xs, t') -> embed funNamesAccum lts1 t' freeVars boundVars renaming) branches
 dived funNamesAccum lts1
-                    lts2@(LTS (LTSTransitions _ branches'@((Let', _) : _))) freeVars boundVars renaming =
+                    lts2@(LTS (LTSTransitions _ branches'@([(Let', t), (LetX' x, u)]))) freeVars boundVars renaming =
     -- if some i has embedding, than dived is passed
-    concatMap (\t -> embed funNamesAccum lts1 t freeVars boundVars renaming) (map snd branches')
-dived _ t u _ _ _ | traceShow ("in dived nothing matched : t = " ++ show t ++ " and u = " ++ show u) False = undefined
+   embed funNamesAccum lts1 t freeVars boundVars renaming ++ embed funNamesAccum lts1 u freeVars boundVars renaming
+--dived _ t u _ _ _ | traceShow ("in dived nothing matched : t = " ++ show t ++ " and u = " ++ show u) False = undefined
+dived _ Leaf Leaf _ _ renaming = [renaming]
 dived _ _ _ _ _ _ = []
 
 
 -- if all i has embedding, than coupled is passed
-coupled :: [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [(String, String)]
---coupled _ t u _ _ _
-  --  | traceShow ("coupled : t = " ++ show t ++ " and u = " ++ show u) False = undefined
+coupled :: [(String, String)] -> LTS -> LTS -> [String] -> [String] -> [(String, String)] -> [[(String, String)]]
+{---coupled _ t u _ _ r
+     | traceShow ("coupled : t = " ++ show t ++ " and u = " ++ show u ++ show r) False = undefined
 coupled _ t@(LTS (LTSTransitions (Free x) _)) u@(LTS (LTSTransitions (Free x') _)) _ _ renaming
-    | traceShow ("coupled : t = " ++ show t ++ " and u = " ++ show u ++ ", r = " ++ show renaming) False = undefined
+    | traceShow ("coupled! : t = " ++ show t ++ " and u = " ++ show u ++ ", r = " ++ show renaming ++ show (if x `elem` map fst renaming
+                                                                                                                                        then [renaming | (x,x') `elem` renaming]--}                                                                                                                                        
 coupled funNamedAccum (LTS (LTSTransitions t@(Free x) _))
                       (LTS (LTSTransitions t'@(Free x') _)) freeVars boundVars renaming =
-                          if x `elem` map fst renaming
-                            then concat ([renaming | (x,x') `elem` renaming])
-                            else concat ([(x,x') : renaming])
-                        {-if  (x,x') `elem` renaming
-                            then renaming
-                            else (x,x') : renaming--}
+                        if x `elem` map fst renaming
+                            then [renaming | (x,x') `elem` renaming]
+                            else [(x,x'):renaming]
 -- unfold
 coupled funNamesAccum (LTS (LTSTransitions (Fun f) _))
                       (LTS (LTSTransitions (Fun g) _)) _ _ renaming | f /= g = []
 coupled funNamesAccum (LTS (LTSTransitions _ [(Unfold' funName, t)]))
                       (LTS (LTSTransitions _ [(Unfold' funName', t')])) freeVars boundVars renaming
-         | (funName, funName') `elem` funNamesAccum = renaming
+         | (funName, funName') `elem` funNamesAccum = [renaming]
          | otherwise = embed ((funName, funName') : funNamesAccum) t t' freeVars boundVars renaming
 -- lambda
 coupled funNamesAccum (LTS (LTSTransitions _ [(Lambda' x, t)]))
-                          (LTS (LTSTransitions _ [(Lambda' x', t')])) freeVars boundVars renaming = let
-        x'' = renameVar freeVars x
-        in embed funNamesAccum t t' (x'' : freeVars) boundVars renaming
+                          (LTS (LTSTransitions _ [(Lambda' x', t')])) freeVars boundVars renaming = embed funNamesAccum t t' freeVars boundVars renaming
+{--coupled funNamesAccum (LTS (LTSTransitions _ [(Con' conName, Leaf)]))
+                          (LTS (LTSTransitions _ [(Con' conName', Leaf)])) freeVars boundVars renaming | conName == conName' = [[("#","#")]]-}
 -- constructor
 coupled funNamesAccum (LTS (LTSTransitions _ bs@((Con' conName, Leaf) : branches)))
                           (LTS (LTSTransitions _ bs'@((Con' conName', Leaf) : branches'))) freeVars boundVars renaming
     | branchesSetsForConstructor bs bs' =
       if conName == conName' then let
         termPairs = zip (map snd branches) (map snd branches')
-        in foldr (\(t, t') renaming' -> embed funNamesAccum t t' freeVars boundVars renaming') renaming termPairs
-        {---embedResults =  map (\(t, t') -> embed funNamesAccum t t' freeVars boundVars renaming) termPairs
-        in if [] `elem` embedResults then [] else concat embedResults--}
+        in foldr (\(t,t') rs -> concat [embed funNamesAccum t t' freeVars boundVars renaming' | renaming' <- rs]) [renaming] termPairs
       else []
 -- Apply
 coupled funNamesAccum (LTS (LTSTransitions (Apply (Fun f) _) [(Apply0', _), (Apply1', u)]))
@@ -153,30 +149,24 @@ coupled funNamesAccum (LTS (LTSTransitions (Apply (Fun f) _) [(Apply0', _), (App
 coupled funNamesAccum (LTS (LTSTransitions (Apply (Fun f) _) [(Apply0', _), (Apply1', u)]))
                                               (LTS (LTSTransitions (Apply (Fun g) _) [(Apply0', _), (Apply1', u')])) _ _ _ | f /= g = []
 coupled funNamesAccum  (LTS (LTSTransitions _ [(Apply0', t), (Apply1', u)]))
-                           (LTS (LTSTransitions _ [(Apply0', t'), (Apply1', u')])) freeVars boundVars renaming = let
-    a = coupled funNamesAccum t t' freeVars boundVars renaming
-    b = embed funNamesAccum u u' freeVars boundVars a in b
+                           (LTS (LTSTransitions _ [(Apply0', t'), (Apply1', u')])) freeVars boundVars renaming =
+   concat [embed funNamesAccum u u' freeVars boundVars renaming' | renaming' <- coupled funNamesAccum t t' freeVars boundVars renaming]
 -- let
 coupled funNamesAccum (LTS (LTSTransitions _ [(Let', t), (LetX' x, u)]))
-                          (LTS (LTSTransitions _ [(Let', t'), (LetX' x', u')])) freeVars boundVars renaming = let
-    x'' = renameVar freeVars x
-    a = embed funNamesAccum t t' freeVars boundVars renaming
-    b = embed funNamesAccum u u' (x'' : freeVars) (x'' : boundVars) renaming in a ++ b
+                          (LTS (LTSTransitions _ [(Let', t'), (LetX' x', u')])) freeVars boundVars renaming =
+                            concat [embed funNamesAccum u u' freeVars boundVars renaming' | renaming' <- embed funNamesAccum t t' freeVars boundVars renaming]
 -- case
-{--coupled funNamesAccum (LTS (LTSTransitions _ ((Case', t) : branches)))
+coupled funNamesAccum (LTS (LTSTransitions _ ((Case', t) : branches)))
                                   (LTS (LTSTransitions _ ((Case', t') : branches'))) freeVars boundVars renaming
-                                  | traceShow ("coupled case, t = " ++ show t ++ ", t' = " ++ show t' ++ show (embed funNamesAccum t t' freeVars boundVars renaming)) False = undefined-}
+                                  | traceShow ("coupled case, t = " ++ show t ++ ", t' = " ++ show t' ++ show (embed funNamesAccum t t' freeVars boundVars renaming)) False = undefined
 coupled funNamesAccum (LTS (LTSTransitions _ ((Case', t) : branches)))
                           (LTS (LTSTransitions _ ((Case', t') : branches'))) freeVars boundVars renaming
     | matchCase branches branches' = let
-        initializer = embed funNamesAccum t t' freeVars boundVars renaming
-        branchess = zip branches branches' 
-        in if null initializer
-            then []
-            else foldr (\((CaseBranch' name xs, u), (CaseBranch' name' xs', u')) renaming' -> let
-                    freeVars' = renameVars freeVars xs
-                    xs'' = take (length xs) freeVars'
-                    in (renaming' ++ embed funNamesAccum u u' freeVars' (xs'' ++ boundVars) renaming')) initializer branchess
-coupled _ t u _ _ _ | traceShow ("IN coupling nothing matched for t = " ++ show t ++ " and u = " ++ show u) False = undefined
+        --initializer = embed funNamesAccum t t' freeVars boundVars renaming
+        branchess = zip branches branches'
+        in foldr (\((CaseBranch' c xs, u),(CaseBranch' c' xs', u')) rs
+            -> concat [embed funNamesAccum u u' freeVars boundVars renaming' | renaming' <- rs]) (embed funNamesAccum t t' freeVars boundVars renaming) branchess
+coupled _ Leaf Leaf _ _ renaming = [renaming]
+--coupled _ t u _ _ _ | traceShow ("IN coupling nothing matched for t = " ++ show t ++ " and u = " ++ show u) False = undefined
 coupled _ _ _ _ _ _ = []
 
--- test/DistillationCorrectness/HEmbeddingCheckerCorrectness.hs
+++ test/DistillationCorrectness/HEmbeddingCheckerCorrectness.hs
@@ -19,7 +19,7 @@ test_checkRenaming_qrev = let
             ,(Apply1', doLTS1Tr (Free "xs") (X' "xs") doLTS0Tr)])
         ,(Apply1', doLTS1Tr (Free "ys") (X' "ys") doLTS0Tr)]
     lts2 = renameVarInLts lts1 ("xs", "xs1")
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "Renaming: qrev xs ys and qrev xs1 ys" $ isRenaming lts1 lts2 @?= [("xs", "xs1")]]
+    in return $ testGroup "HEChecker" [testCase "Renaming: qrev xs ys and qrev xs1 ys" $ isRenaming lts1 lts2 @?= [("xs", "xs1")]]
 
 test_checkRenaming_lambda :: IO TestTree
 test_checkRenaming_lambda = let
@@ -35,76 +35,92 @@ test_checkRenaming_lambda = let
                             (Free "xs"))
                         (Con "Nil" []))) [] []
     lts2 = renameVarInLtsRecursively [("xs", "xs1"), ("ys", "ys1")] lts1
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "Renaming: lambda x ys xs x xs and lambda x ys1 xs1 x xs1" $ isRenaming lts1 lts2 @?= [("xs", "xs1"), ("ys", "ys1")]]
+    in return $ testGroup "HEChecker" [testCase "Renaming: lambda x ys xs x xs and lambda x ys1 xs1 x xs1" $ isRenaming lts1 lts2 @?= [("xs", "xs1"), ("ys", "ys1")]]
 
 test_checkEmbedding_qrev :: IO TestTree
 test_checkEmbedding_qrev = let
     lts1 = qrevLts
     lts2 = qrevLts'
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "HEmbedding: qrev" $ isHomeomorphicEmbedding lts2 lts1 @?= [("x","x"),("x","x'"),("xs","xs")]]
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: qrev lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= [("x","x"),("xs","xs"),("x","x'"),("xs","xs")]
+                                      ,testCase "HEmbedding: qrev lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= []]
     
 test_checkEmbedding_swap_qrev :: IO TestTree
 test_checkEmbedding_swap_qrev = let 
     lts1 = swapQrevLts
     lts2 = swapQrevLts'
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "HEmbedding: swap qrev" $ isHomeomorphicEmbedding lts2 lts1 @?= [("y","y"),("y","y'"),("ys","ys")]]
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: swap qrev lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= [("y","y"),("ys","ys"),("y","y'"),("ys","ys")]
+                                      ,testCase "HEmbedding: swap qrev lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= []]
 
 test_checkEmbedding_neil3 :: IO TestTree
 test_checkEmbedding_neil3 = let
     lts1 = neil3Lts
     lts2 = neil3Lts'
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "HEmbedding: neil3" $ isHomeomorphicEmbedding lts1 lts2 @?= [("xs","xs"),("xs'","x'"),("xs'","xs"),("xs'","xs''")]]
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: neil3 lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= [("xs'","xs''"),("xs","xs")]
+                                      ,testCase "HEmbedding: neil3 lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= []]
 
 test_checkEmbedding_append :: IO TestTree
 test_checkEmbedding_append = let
     lts1 = appendLts
     lts2 = appendLts'
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "HEmbedding: append" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x'","x''"),("xs","x'"),("xs","xs'"),("xs","xs''"),("xs'","xs''")]]
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: append lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x'","x''"),("xs'","xs''"),("xs","xs'")]
+                                      ,testCase "HEmbedding: append lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= []]
     
 test_checkEmbedding_revrev :: IO TestTree
 test_checkEmbedding_revrev = let 
-    --lts1 = revrevTermLts
-    --lts2 = revrevTermLts'
-    {--lts1 = drive (Case (Apply (Apply (Fun "append") (Free "xs'"))
+    lts111111 = revrevTermLts
+    lts222222 = revrevTermLts'
+    lts11111 = drive (Case (Apply (Apply (Fun "append") (Free "xs'"))
                         (Con "Con" [Free "x'", Con "Nil" []]))
                    [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
                    ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
-    lts2 = drive (Case (Apply (Apply (Fun "append") (Free "xs''"))
+    lts22222 = drive (Case (Apply (Apply (Fun "append") (Free "xs''"))
                     (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]]))
                    [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
-                   ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []      -}
-    {--lts1 = drive (Case (Con "Con" [Free "x'", Con "Nil" []])
+                   ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
+    lts1111 = drive (Case (Con "Con" [Free "x'", Con "Nil" []])
                                     [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
                                     ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
-    lts2 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
-                                    [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []])) ---------------------[[("x","x"),("xs","xs"),("x'","x'")]]
-                                    ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []  -}
-    {---lts1 = drive (Case (Con "Con" [Free "x'", Con "Nil" []])
+    lts2222 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
+                                    [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
+                                    ,("Con",["x","xs"], Con "Con" [Free "x", Apply (Apply (Fun "append") (Free "xs")) (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
+    lts111 = drive (Case (Con "Con" [Free "x'", Con "Nil" []])
                                         [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
                                         ,("Con",["x","xs"], Con "Con" [Free "x", (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
-    lts2 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
+    lts222 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
                                         [("Nil", [], (Con "Con" [Free "x'", Con "Nil" []]))
-                                        ,("Con",["x","xs"], Con "Con" [Free "x", (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []  --} ---------[[("x","x"),("x'","x'")]]  
-    {---lts1 = drive (Case (Con "Con" [Free "x'", Con "Nil" []]) ------------- [[("x'","x'")]]
+                                        ,("Con",["x","xs"], Con "Con" [Free "x", (Con "Con" [Free "x'", Con "Nil" []])])]) ["append"] []
+    lts11 = drive (Case (Con "Con" [Free "x'", Con "Nil" []])
                     [("Nil", [], Con "Nil" [])
                     ,("Con",["x","xs"], (Con "Con" [Free "x'", Con "Nil" []]))])    [] []
-    lts2 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
+    lts22 = drive (Case (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])
                     [("Nil", [], Con "Nil" [])
-                    ,("Con",["x","xs"], (Con "Con" [Free "x'", Con "Nil" []]))])   [] []        --}
-    lts1 = drive ((Apply (Apply (Fun "f") ((Con "Con" [Free "x'", Con "Nil" []]))) ((Free "x'")))) ["f"] []
-    lts2 = drive ((Apply (Apply (Fun "f") (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])) ((Free "x'")))) ["f"] []                                                                                                                                                                                           
-    in return $ testGroup "HEChecker" [testCase "HEmbedding: revrev" $ isHomeomorphicEmbedding lts1 lts2 @?= []]
+                    ,("Con",["x","xs"], (Con "Con" [Free "x'", Con "Nil" []]))])   [] []
+    lts1 = drive ((Apply (Apply (Fun "f") ((Con "Con" [Free "x'", Con "Nil" []]))) ((Free "x''")))) ["f"] []
+    lts2 = drive ((Apply (Apply (Fun "f") (Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])) ((Free "x''")))) ["f"] []
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: revrev lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x''","x''"),("x'","x'"),("x''","x''"),("x'","x''")]
+                                      ,testCase "HEmbedding: revrev lts11 lts22" $ isHomeomorphicEmbedding lts11 lts22 @?= [("x'","x'")]
+                                      ,testCase "HEmbedding: revrev lts111 lts222" $ isHomeomorphicEmbedding lts111 lts222 @?= [("x", "x"), ("x'","x'")]
+                                      ,testCase "HEmbedding: revrev lts1111 lts2222" $ isHomeomorphicEmbedding lts1111 lts2222 @?= [("x","x"),("xs","xs"),("x'","x'")]
+                                      ,testCase "HEmbedding: revrev lts11111 lts22222" $ isHomeomorphicEmbedding lts11111 lts22222 @?= [("x","x"),("xs","xs"),("x'","x'"),("xs'","xs''")]
+                                      ,testCase "HEmbedding: revrev lts111111 lts222222" $ isHomeomorphicEmbedding lts111111 lts222222 @?= [("x","x"),("xs","xs"),("x'","x'"),("xs'","xs''")]
+                                      ,testCase "HEmbedding: revrev lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= []
+                                      ,testCase "HEmbedding: revrev lts22 lts11" $ isHomeomorphicEmbedding lts22 lts11 @?= []
+                                      ,testCase "HEmbedding: revrev lts222 lts111" $ isHomeomorphicEmbedding lts222 lts111 @?= []
+                                      ,testCase "HEmbedding: revrev lts2222 lts1111" $ isHomeomorphicEmbedding lts2222 lts1111 @?= []
+                                      ,testCase "HEmbedding: revrev lts22222 lts11111" $ isHomeomorphicEmbedding lts22222 lts11111 @?= []
+                                      ,testCase "HEmbedding: revrev lts222222 lts111111" $ isHomeomorphicEmbedding lts222222 lts111111 @?= []]
 
 test_checkEmbedding_nested_cases :: IO TestTree
 test_checkEmbedding_nested_cases = let
     lts1 = term1Lts
     lts2 = term2Lts
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]{---[testCase "HEmbedding: nested cases" $ isHomeomorphicEmbedding lts2 lts1 @?= [("v'","v'"),("vs'","vs'"),("x'","x'"),("xs'","xs''")]
-                                      ,testCase "HEmbedding: nested cases" $ isHomeomorphicEmbedding lts1 lts2 @?= []]--}
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: nested cases lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x'","x'"),("v'","v'"),("vs'","vs'"),("xs'","xs''")]
+                                      ,testCase "HEmbedding: nested cases lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= []]
                                       
 test_check_nested_constructors :: IO TestTree
 test_check_nested_constructors = let 
     lts1 = drive ((Con "Con" [Free "x'", Con "Nil" []])) [] []
     lts2 = drive ((Con "Con" [Free "x''", Con "Con" [Free "x'", Con "Nil" []]])) [] []
-    in return $ testGroup "HEChecker" [testCase "la" $ 2 + 2 @?= 4]--[testCase "HEmbedding: nested constructors" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x'", "x''")]] 
+    in return $ testGroup "HEChecker" [testCase "HEmbedding: nested constructors lts1 lts2" $ isHomeomorphicEmbedding lts1 lts2 @?= [("x'", "x''")]
+                                      ,testCase "HEmbedding: nested constructors lts2 lts1" $ isHomeomorphicEmbedding lts2 lts1 @?= []]
                                                                                     
